2bd7d8ac8cf090d1768818ae1b144e5c2953e5a9
Leon-Leyang <scylh6@nottingham.edu.cn>
2023-05-07 15:26:23 +0800

    Add `clean_data` to clean the data

--- utils/preprocess/clean_agg_vec_new.py
+++ utils/preprocess/clean_agg_vec_new.py
@@ -1,11 +1,12 @@
 import getpass
 import pickle
 from hdfs import InsecureClient
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import udf, col, lag
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import MinMaxScaler, VectorAssembler
-from pyspark.sql.types import ArrayType, DoubleType
+from pyspark.sql.types import ArrayType, DoubleType, IntegerType, StringType
+from globals import *
 
 
 # Get the username
@@ -16,6 +17,41 @@ hdfs_client = InsecureClient('http://localhost:9870', user=username)
 
 # Initialize the spark session
 spark = SparkSession.builder.appName("Vector cleaner and aggregator").getOrCreate()
+
+
+# Function that returns the index of the geohash
+def geo_2_idx(geohash_to_idx, geohash):
+    return geohash_to_idx[geohash]
+
+
+# Function that returns 1 if the day of the week is a weekday, 0 otherwise
+def get_week_day(dow):
+    if dow < 5:
+        return 1
+    else:
+        return 0
+
+
+# Function that returns the time interval of the day
+def get_time_interval(hod):
+    if 6 <= hod < 10:
+        return 0
+    if 10 <= hod < 15:
+        return 1
+    if 15 <= hod < 18:
+        return 2
+    if 18 <= hod < 22:
+        return 3
+    else:
+        return 4
+
+
+# Function that returns 1 if the value is greater than 0, 0 otherwise
+def make_binary(d):
+    if d > 0:
+        return 1
+    else:
+        return 0
 
 
 # Function to process the poi data
@@ -89,6 +125,39 @@ def convert_nlp_df2dict(nlp_path):
         pickle.dump(NLP_dict, writer)
 
 
+# Function to clean the data
+def clean_data(c):
+    df = spark.read.csv(f'hdfs://localhost:9000/data/temp/{c}_geo2vec.csv', header=True, inferSchema=True)
+
+    with hdfs_client.read(f'/data/temp/geo2idx.pickle') as reader:
+        geohash_to_idx = pickle.load(reader)
+
+    geo_2_idx_udf = udf(lambda x: geo_2_idx(geohash_to_idx, x), StringType())
+    get_week_day_udf = udf(get_week_day, IntegerType())
+    get_time_interval_udf = udf(get_time_interval, IntegerType())
+    make_binary_udf = udf(make_binary, IntegerType())
+
+    df = df.withColumn("geohash_code", geo_2_idx_udf(df["Geohash"]))
+    df = df.withColumn("DOW_cat", get_week_day_udf(df["DOW"]))
+    df = df.withColumn("HOD_cat", get_time_interval_udf(df["HOD"]))
+    df = df.withColumn("T-Accident", make_binary_udf(df["T-Accident"]))
+
+    w = Window.partitionBy("Geohash").orderBy("TimeStep")
+    df = df.withColumn("predicted_accident", lag("T-Accident", -1).over(w))
+    df = df.dropna(subset=("predicted_accident",))
+
+    # Select the desired columns
+    selected_columns = [
+        'TimeStep', 'predicted_accident', 'Geohash', 'geohash_code', 'HOD_cat', 'DOW_cat', 'T-Accident', 'DayLight',
+        'T-BrokenVehicle', 'T-Congestion', 'T-Construction', 'T-Event', 'T-FlowIncident', 'T-Other', 'T-RoadBlocked',
+        'W-Humidity', 'W-Precipitation', 'W-Pressure', 'W-Temperature', 'W-Visibility', 'W-WindSpeed', 'W-Rain',
+        'W-Snow', 'W-Fog', 'W-Hail'
+    ]
+    df = df.select(selected_columns)
+
+    df.write.csv(f'hdfs://localhost:9000/data/temp/{c}_geo2vec_cleaned.csv', header=True, mode="overwrite")
+
+
 if __name__ == '__main__':
     # Process the POI data
     poi_path = "hdfs://localhost:9000/data/geohash_to_poi_vec.csv"
@@ -97,3 +166,7 @@ if __name__ == '__main__':
     # Process the NLP data
     nlp_path = "hdfs://localhost:9000/data/temp/geohash_to_text_vec.csv"
     convert_nlp_df2dict(nlp_path)
+
+    # Clean the data
+    for c in cities:
+        clean_data(c)
